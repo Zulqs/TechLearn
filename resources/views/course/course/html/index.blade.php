16764b41492aec63c0de8e28a7530e7b1f499d07
lilynlilo <luhagustinaaaryani@gmail.com>
2024-06-04 21:21:34 +0700

    html

--- resources/views/course/course/html/index.blade.php
+++ resources/views/course/course/html/index.blade.php
@@ -29,8 +29,15 @@
                 </svg>
                 <div class="px-3 text-lg" >Introduction</div>
             </a>
+            <a class="flex flex-row p-3 my-2 bg-btr rounded-lg" href="{{url('/course/html/2')}}">
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4"/>
+                </svg>
+                <div class="px-3 text-lg" >Editors</div>
+            </a>
         </div>
     </div>
+    
 
     <div class="container max-w-[1110px] bg-white rounded-lg shadow-sm">
         <div class="flex flex-col p-3 my-4 text-white">
@@ -47,5 +54,4 @@
                 <div class="px-3 text-lg" >HTML Test</div>
             </a>
         </div>
-    </div>
 </x-app-layout>

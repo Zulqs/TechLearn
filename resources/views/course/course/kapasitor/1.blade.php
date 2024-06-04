1fefeb09f061ec328345e31ce4fa4eafe64b4217
lilynlilo <luhagustinaaaryani@gmail.com>
2024-06-04 21:12:00 +0700

    kapasitor

---
+++ resources/views/course/course/kapasitor/1.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('kapasitor Introduction') }}
+    </h2>
+  </x-slot>
+
+  <div class="container max-w-[1110px] my-8 bg-white rounded-lg shadow-sm">
+    <div class="flex flex-col p-6">
+      <div class="flex flex-row border-y py-4">
+        <div class="px-3">Kapasitor termasuk komponen elektronik</div>
+      </div>
+      <div class="py-4 border-b">
+
+        <div class="px-3 font-bold text-3xl">Apa itu kapasitor?</div>
+        <ul style="list-style-type:circle;" class="px-12 py-5">
+          <div class="py4">Kapasitor merupakan komponen elektronik yang berfungsi untuk menyimpan muatan listrik. Dalam dunia elektronika, kapasitor biasa dilambangkan sebagai C. 
+          <div>Ilmuwan yang pertama kali menemukan adanya kapasitor adalah Michael Faraday. Itulah mengapa nama Faraday disematkan sebagai satuan dari kapasitansi, yaitu Farad (F).
+          </ul>
+        </div>  
+      <div class="py-4 border-b">
+
+        <div class="px-3 font-bold text-3xl">Prinsip Kerja Kapasitor</div>
+        <ul style="list-style-type:number;" class="px-12 py-5">
+        <img src="https://akupintar.id/documents/20143/0/satuan+kapasitor.jpg/f7743709-1aae-22a2-04cc-7eef84fce869?t=1694267594583&imagePreview=1" alt="Deskripsi Gambar" width="500" >
+          <div class="py-4">Di atas adalah gambar kapasitor. Struktur atau komponen kapasitor terdiri dari dua buah lempengan logam (pelat metal) yang disusun secara paralel dan berdekatan satu sama lain. 
+          <div class="py-4">Kedua lempengan metal tersebut dipisahkan oleh suatu bahan dielektrik. Jarak antara kedua lempengan disebut sebagai jarak dielektrik.
+          <div class="py-4">Proses pengisian kapasitor atau kondensator dimulai ketika kedua ujung pelat metal diberi tegangan listrik. 
+          Muatan-muatan positif akan mengumpul pada salah satu kaki (elektroda) metal dan, pada saat yang sama, muatan-muatan negatif terkumpul pada ujung metal yang satu lagi. 
+          Karena terpisah oleh bahan dielektrik yang nonkonduktif, muatan positif tidak dapat mengalir menuju ujung kutub negatif dan muatan negatif tidak bisa menuju ke ujung kutub positif.
+          <div class="py-4"> Ketika elektron-elektron mengalir dari sumber tegangan ke salah satu lempengan kapasitor, lempengan lainnya kehilangan elektron. Akibatnya, terjadilah ketidakseimbangan muatan listrik antara kedua lempengan. 
+          Muatan listrik ini akan terus tersimpan di dalam kapasitor selama jangka waktu tertentu. Prinsip kerja kapasitor diuraikan sebagai berikut:
+          <li>Dua pelat atau lebih dalam kondisi berhadapan, kemudian pelat tersebut dibatasi oleh penyekat. Ketika masing-masing pelat dialiri listrik, akan terbentuklah kondensator.
+          <li>Nilai kapasitas dari sebuah kapasitor dipengaruhi oleh kedua pelat yang saling berhadapan, bahan dielektrum, serta jarak antara kedua pelat tersebut.
+          <li>Kapasitansi panic terjadi apabila adanya komponen-komponen yang saling berdekatan, kemudian menyebabkan terjadinya kapasitor liar karena terpisah oleh bahan dielektrik yang nonkonduktif.
+        </ul>
+</x-app-layout>
---
+++ resources/views/course/course/kapasitor/2.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('PHP Editors') }}
+    </h2>
+  </x-slot>
+
+  <div class="container max-w-[1110px] my-8 bg-white rounded-lg shadow-sm">
+    <div class="flex flex-col p-6">
+      <div class="flex flex-row border-y py-4">
+        <div class="px-3">Kapasitor termasuk komponen elektronik</div>
+      </div>
+      <div class="py-4 border-b">
+        <div class="px-3 font-bold text-3xl">Cara Kerja dan Fungsi Kapasitor</div>
+        <ul style="list-style-type:number;" class="px-12 py-5">
+          <div class="px-3 text-4m">Fungsi kapasitor cukup penting, terutama dalam menjaga kinerja dan stabilitas rangkaian listrik dan elektronika. Kapasitor juga memiliki peranan lain untuk memenuhi berbagai kebutuhan di dunia elektronika.
+          <div  class="py-4">Fungsi kapasitor lebih jauh dijelaskan sebagai berikut:
+          <li>Menyimpan Muatan Listrik (Kapasitansi) 
+          <div>Fungsi kapasitor yang utama adalah untuk menyimpan energi listrik. 
+          <li>Kapasitor dapat menyimpan muatan listrik dalam jangka waktu tertentu dan melepaskannya saat diperlukan dalam rangkaian.
+          <div>Filter atau Penyaring dalam Rangkaian Power Supply
+          <div>Dalam rangkaian catu daya, kegunaan kapasitor adalah sebagai filter untuk menyaring komponen frekuensi yang tidak diinginkan atau noise dari sumber tegangan. Dengan begitu, tegangan yang dihasilkan oleh kapasitor menjadi lebih stabil dan bebas dari gangguan.
+          <li>Pembangkit Frekuensi pada Alat Osilator
+          <div>Kegunaan kapasitor yang lainnya yaitu berperan sebagai pembangkit frekuensi dalam rangkaian osilator. Ketika digabungkan dengan resistor dan komponen lainnya, kapasitor dapat menghasilkan osilasi atau gelombang dengan frekuensi tertentu.
+          <li>Menyimpan Tegangan dan Kuat Arus pada Periode Tertentu
+          <div>Kapasitor dapat digunakan untuk menyimpan tegangan dan kuat arus dalam jangka waktu tertentu. Ketika digunakan dalam rangkaian, fungsi kapasitor adalah sebagai penyimpan energi, penunda waktu, dan aplikasi pengaturan daya.
+          <li>Frekuensi pada Rangkaian Antena
+          <div>Kapasitor digunakan dalam rangkaian antena untuk mengatur frekuensi sinyal yang diterima atau dikirim. Dalam hal ini, fungsi kapasitor adalah sebagai filter frekuensi, sehingga sinyal dengan frekuensi tertentu saja yang dapat lewat.
+          <li>Penghemat daya Listrik pada Lampu Neon
+          <div>Kegunaan kapasitor berikutnya adalah untuk menghemat daya listrik dalam rangkaian lampu neon. Kapasitor membantu mengatur tegangan dan mengurangi kerugian daya yang terjadi selama lampu neon menyala.
+          <li>Penghilang Loncatan Api (Bouncing) pada Saklar
+          <div>Kapasitor dapat digunakan untuk menghilangkan loncatan api yang terjadi saat memasang atau melepaskan saklar. Fungsi kapasitor adalah membantu menjaga stabilitas sinyal dan mengurangi efek bouncing yang tidak diinginkan.
+          <li>Kopling, Penggeser Fasa, dan Konduktor
+          <div>Fungsi kapasitor yang terakhir adalah sebagai kopling antara dua rangkaian dan menggeser fasa sinyal dalam rangkaian. Kapasitor juga berfungsi sebagai konduktor dalam rangkaian arus bolak-balik.
+</x-app-layout>

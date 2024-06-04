7eb0c8dc17e66820f41b24af8f33f3e4b443aad9
lilynlilo <luhagustinaaaryani@gmail.com>
2024-06-04 17:17:37 +0700

    php

--- resources/views/course/course/html/1.blade.php
+++ resources/views/course/course/html/1.blade.php
@@ -1,74 +1,114 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('kapasitor Introduction') }}
+      {{ __('HTML Introduction') }}
     </h2>
   </x-slot>
 
   <div class="container max-w-[1110px] my-8 bg-white rounded-lg shadow-sm">
     <div class="flex flex-col p-6">
       <div class="flex flex-row border-y py-4">
-        <div class="px-3">Kapasitor termasuk barang elektronika</div>
+        <div class="px-3">HTML adalah markup languange standard untuk membuat halaman Web.</div>
+      </div>
+      <div class="py-4 border-b">
+        <div class="px-3 font-bold text-3xl">Apa itu HTML?</div>
+        <ul style="list-style-type:circle;" class="px-12 py-5">
+          <li>HTML adalah singkatan dari Hyper Text Markup Language</li>
+          <li>HTML adalah markup languange standard untuk membuat halaman Web.</li>
+          <li>HTML mengartikan sebuah struktur dari halaman Web</li>
+          <li>HTML terdiri dari serangkaian elemen</li>
+          <li>Elemen HTML memberi informasi kepada browser bagaimana content harus ditampilkan</li>
+          <li>Elemen HTML melabeli potongan konten seperti "ini adalah judul", "ini adalah paragraf", "ini adalah tautan", dll.</li>
+        </ul>
       </div>
       <div class="py-4 border-b">
 
-        <div class="px-3 font-bold text-3xl">Apa itu kapasitor?</div>
+        <div class="px-3 font-bold text-3xl">Dokumen HTML yang Sederhana</div>
+        <ul style="list-style-type:circle;" class="px-8 py-5">
+          <p style="background:lightgrey;border-radius: 10px; padding: 20px;" class="text-3xl">Contoh</p>
+        </ul>
+
+        <div class="px-4 text-3xl">Penjelasan Contoh</div>
         <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>kapasitor adalah singkatan dari “kapasitor: Hypertext Preprocessor”</li>
-          <li>kapasitor adalah bahasa skrip sumber terbuka yang digunakan secara luas</li>
-          <li>Skrip kapasitor dieksekusi di server</li>
-          <li>kapasitor gratis untuk diunduh dan digunakan</li>
+          <li>Deklarasi tersebut <a style="color: red;"> &lt;!DOCTYPE html&gt; </a>mendefinisikan bahwa dokumen ini adalah dokumen HTML5</li>
+          <li>Elemen <a style="color: red;">&lt;hml&gt;</a> adalah elemen akar dari halaman HTML</li>
+          <li>Elemen tersebut <a style="color: red;">&lt;head&gt; </a> menentukan judul untuk halaman HTML (yang ditampilkan di bilah judul browser atau di tab halaman)</li>
+          <li>Elemen <a style="color: red;">&lt;body&gt; </a> mendefinisikan badan dokumen, dan merupakan wadah untuk semua konten yang terlihat, seperti judul, paragraf, gambar, hyperlink, tabel, daftar, dll.</li>
+          <li>Elemen <a style="color: red;">&lt;h1&gt; </a> mendefinisikan judul besar</li>
+          <li>Elemen <a style="color: red;">&lt;p&gt; </a> mendefinisikan paragraf</li>
         </ul>
       </div>
+      <div class="py-3 border-b">
+
+        <div class="px-3 py-5 font-bold text-3xl">Apa itu elemen HTML?</div>
+
+        <div class="px-3 text-4m">Elemen HTML ditentukan oleh tag awal, beberapa konten, dan tag akhir:</div>
+        <div class="px-5 py-6 text-2xl"><a style="color: blue;">&lt;</a><a style="color: brown;">tagname</a><a style="color: blue;">&gt;</a> Konten ada di sini... <a style="color: blue;">&lt;</a><a style="color: brown;">/tagname</a><a style="color: blue;">&gt;</a></div>
+        <div class="px-3 text-4m"><b> Elemen </b> HTML adalah segalanya mulai dari tag awal hingga tag akhir:</div>
+        <div class="px-5 py-5 text-2xl"><a style="color: blue;">&lt;</a><a style="color: brown;">h1</a><a style="color: blue;">&gt;</a> Judul Pertama <a style="color: blue;">&lt;</a><a style="color: brown;">/h1</a><a style="color: blue;">&gt;</a></div>
+        <div class="px-5 py-5 text-2xl"><a style="color: blue;">&lt;</a><a style="color: brown;">p</a><a style="color: blue;">&gt;</a> Paragraf Pertama <a style="color: blue;">&lt;</a><a style="color: brown;">/p</a><a style="color: blue;">&gt;</a></div>
+      </div>
+
+      <style>
+        table {
+          font-family: arial, sans-serif;
+          border-collapse: collapse;
+          width: 100%;
+        }
+
+        td,
+        th {
+          border: 1px solid #dddddd;
+          text-align: left;
+          padding: 8px;
+        }
+
+        tr:nth-child(even) {
+          background-color: #dddddd;
+        }
+      </style>
+      </head>
+      <table>
+        <tr>
+          <th>Start tag</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>&lt;h1&gt;</td>
+          <td>Judul Pertama</td>
+          <td>&lt;/h1&gt;</td>
+        </tr>
+        <tr>
+          <td>&lt;p&gt;</td>
+          <td>Paragraf Pertama</td>
+          <td>&lt;/p&gt;</td>
+        </tr>
+        <tr>
+          <td>&lt;br&gt;</td>
+          <td><i>none</i></td>
+          <td><i>none</i></td>
+        </tr>
+      </table>
     </div>
-    <p style="background:lightyellow; padding: 30px;" class="text-3l"><b>kapasitor adalah bahasa yang luar biasa dan populer!</b> Bahasa ini cukup kuat untuk menjadi inti dari sistem blog terbesar di web (WordPress)!
-      Bahasa ini cukup dalam untuk menjalankan jaringan sosial yang besar!
-      kapasitor juga cukup mudah untuk menjadi bahasa pemrograman sisi server pertama bagi pemula!</p>
+
+    <p style="background:lightyellow; padding: 30px;" class="text-3l"><b>Catatan:</b> Beberapa elemen HTML tidak memiliki konten (seperti elemen &lt;br&gt;). Elemen-elemen ini disebut elemen kosong. Elemen kosong tidak memiliki tag akhir!</p>
     <div class="flex flex-col p-6">
-      <div class="py-4 border-b">
 
-        <div class="px-3 font-bold text-3xl">Apa yang dimaksud dengan file kapasitor?</div>
-        <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>File kapasitor dapat berisi teks, HTML, CSS, JavaScript, dan kode kapasitor<< /li>
-          <li>Kode kapasitor dieksekusi di server, dan hasilnya dikembalikan ke browser sebagai HTML biasa</li>
-          <li>File kapasitor memiliki ekstensi “.kapasitor”</li>
-        </ul>
+      <div class="py-1  border-b">
       </div>
-      <div class="py-4 border-b">
 
-        <div class="px-3 font-bold text-3xl">Apa yang dimaksud dengan file kapasitor?</div>
-        <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>File kapasitor dapat berisi teks, HTML, CSS, JavaScript, dan kode kapasitor</li>
-          <li>kapasitor dapat membuat, membuka, membaca, menulis, menghapus, dan menutup file di server</li>
-          <li>kapasitor dapat mengumpulkan data formulir</li>
-          <li>kapasitor dapat mengirim dan menerima cookie</li>
-          <li>kapasitor dapat menambah, menghapus, memodifikasi data dalam database Anda</li>
-          <li>kapasitor dapat digunakan untuk mengontrol akses pengguna</li>
-          <li>kapasitor dapat mengenkripsi data</li>
-          <div> Dengan kapasitor, Anda tidak terbatas pada output HTML. Anda dapat menampilkan gambar atau file PDF. Anda juga dapat menghasilkan teks apa pun, seperti XHTML dan XML.</div>
+      <div class="py-6 border-b">
       </div>
-      <div class="py-4 border-b">
 
+      <div class="px-3 py-4 font-bold text-3xl">Web Browsers</div>
+      <div class="px-3 text-4m">Tujuan browser web (Chrome, Edge, Firefox, Safari) adalah untuk membaca dokumen HTML dan menampilkannya dengan benar.
+        Browser tidak menampilkan tag HTML, namun menggunakannya untuk menentukan cara menampilkan dokumen:</div>
+      <ul style="list-style-type:circle;" class="px-12 py-5">
+        <img src="https://www.w3schools.com/html/img_chrome.png" alt="Deskripsi Gambar">
 
-        <div class="px-3 font-bold text-3xl">Mengapa kapasitor?</div>
-        <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>kapasitor berjalan di berbagai platform (Windows, Linux, Unix, Mac OS X, dll.)</li>
-          <li>kapasitor kompatibel dengan hampir semua server yang digunakan saat ini (Apache, IIS, dll.)</li>
-          <li>kapasitor mendukung berbagai macam database</li>
-          <li>kapasitor gratis. Unduh dari sumber daya resmi kapasitor: www.kapasitor.net</li>
-          <li>kapasitor mudah dipelajari dan berjalan secara efisien di sisi server</li>
-          <div> Dengan kapasitor, Anda tidak terbatas pada output HTML. Anda dapat menampilkan gambar atau file PDF. Anda juga dapat menghasilkan teks apa pun, seperti XHTML dan XML.</div>
-      </div>
-      <div class="py-4 border-b">
-
-        <div class="px-3 font-bold text-3xl">Apa yang baru dalam kapasitor 7</div>
-        <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>kapasitor 7 jauh lebih cepat daripada rilis stabil populer sebelumnya (kapasitor 5.6)</li>
-          <li>kapasitor 7 telah meningkatkan Penanganan Kesalahan</li>
-          <li>kapasitor 7 mendukung Deklarasi Tipe yang lebih ketat untuk argumen fungsi</li>
-          <li>kapasitor 7 mendukung operator baru (seperti operator pesawat ruang angkasa: <=>)</li>
-      </div>
-      <div class="py-4 border-b">
-      </div>
     </div>
+
+  </div>
+  </div>
 </x-app-layout>
--- resources/views/course/course/php/1.blade.php
+++ resources/views/course/course/php/1.blade.php
@@ -1,114 +1,74 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('HTML Introduction') }}
+      {{ __('PHP Introduction') }}
     </h2>
   </x-slot>
 
   <div class="container max-w-[1110px] my-8 bg-white rounded-lg shadow-sm">
     <div class="flex flex-col p-6">
       <div class="flex flex-row border-y py-4">
-        <div class="px-3">HTML adalah markup languange standard untuk membuat halaman Web.</div>
-      </div>
-      <div class="py-4 border-b">
-        <div class="px-3 font-bold text-3xl">Apa itu HTML?</div>
-        <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>HTML adalah singkatan dari Hyper Text Markup Language</li>
-          <li>HTML adalah markup languange standard untuk membuat halaman Web.</li>
-          <li>HTML mengartikan sebuah struktur dari halaman Web</li>
-          <li>HTML terdiri dari serangkaian elemen</li>
-          <li>Elemen HTML memberi informasi kepada browser bagaimana content harus ditampilkan</li>
-          <li>Elemen HTML melabeli potongan konten seperti "ini adalah judul", "ini adalah paragraf", "ini adalah tautan", dll.</li>
-        </ul>
+        <div class="px-3">Kode PHP dieksekusi di server.</div>
       </div>
       <div class="py-4 border-b">
 
-        <div class="px-3 font-bold text-3xl">Dokumen HTML yang Sederhana</div>
-        <ul style="list-style-type:circle;" class="px-8 py-5">
-          <p style="background:lightgrey;border-radius: 10px; padding: 20px;" class="text-3xl">Contoh</p>
-        </ul>
-
-        <div class="px-4 text-3xl">Penjelasan Contoh</div>
+        <div class="px-3 font-bold text-3xl">Apa itu PHP?</div>
         <ul style="list-style-type:circle;" class="px-12 py-5">
-          <li>Deklarasi tersebut <a style="color: red;"> &lt;!DOCTYPE html&gt; </a>mendefinisikan bahwa dokumen ini adalah dokumen HTML5</li>
-          <li>Elemen <a style="color: red;">&lt;hml&gt;</a> adalah elemen akar dari halaman HTML</li>
-          <li>Elemen tersebut <a style="color: red;">&lt;head&gt; </a> menentukan judul untuk halaman HTML (yang ditampilkan di bilah judul browser atau di tab halaman)</li>
-          <li>Elemen <a style="color: red;">&lt;body&gt; </a> mendefinisikan badan dokumen, dan merupakan wadah untuk semua konten yang terlihat, seperti judul, paragraf, gambar, hyperlink, tabel, daftar, dll.</li>
-          <li>Elemen <a style="color: red;">&lt;h1&gt; </a> mendefinisikan judul besar</li>
-          <li>Elemen <a style="color: red;">&lt;p&gt; </a> mendefinisikan paragraf</li>
-        </ul>
+          <li>PHP adalah singkatan dari “PHP: Hypertext Preprocessor”</li>
+          <li>PHP adalah bahasa skrip sumber terbuka yang digunakan secara luas</li>
+          <li>Skrip PHP dieksekusi di server</li>
+          <li>PHP gratis untuk diunduh dan digunakan</li>
+          </ul>
       </div>
-      <div class="py-3 border-b">
-
-        <div class="px-3 py-5 font-bold text-3xl">Apa itu elemen HTML?</div>
-
-        <div class="px-3 text-4m">Elemen HTML ditentukan oleh tag awal, beberapa konten, dan tag akhir:</div>
-        <div class="px-5 py-6 text-2xl"><a style="color: blue;">&lt;</a><a style="color: brown;">tagname</a><a style="color: blue;">&gt;</a> Konten ada di sini... <a style="color: blue;">&lt;</a><a style="color: brown;">/tagname</a><a style="color: blue;">&gt;</a></div>
-        <div class="px-3 text-4m"><b> Elemen </b> HTML adalah segalanya mulai dari tag awal hingga tag akhir:</div>
-        <div class="px-5 py-5 text-2xl"><a style="color: blue;">&lt;</a><a style="color: brown;">h1</a><a style="color: blue;">&gt;</a> Judul Pertama <a style="color: blue;">&lt;</a><a style="color: brown;">/h1</a><a style="color: blue;">&gt;</a></div>
-        <div class="px-5 py-5 text-2xl"><a style="color: blue;">&lt;</a><a style="color: brown;">p</a><a style="color: blue;">&gt;</a> Paragraf Pertama <a style="color: blue;">&lt;</a><a style="color: brown;">/p</a><a style="color: blue;">&gt;</a></div>
       </div>
-
-      <style>
-        table {
-          font-family: arial, sans-serif;
-          border-collapse: collapse;
-          width: 100%;
-        }
-
-        td,
-        th {
-          border: 1px solid #dddddd;
-          text-align: left;
-          padding: 8px;
-        }
-
-        tr:nth-child(even) {
-          background-color: #dddddd;
-        }
-      </style>
-      </head>
-      <table>
-        <tr>
-          <th>Start tag</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>&lt;h1&gt;</td>
-          <td>Judul Pertama</td>
-          <td>&lt;/h1&gt;</td>
-        </tr>
-        <tr>
-          <td>&lt;p&gt;</td>
-          <td>Paragraf Pertama</td>
-          <td>&lt;/p&gt;</td>
-        </tr>
-        <tr>
-          <td>&lt;br&gt;</td>
-          <td><i>none</i></td>
-          <td><i>none</i></td>
-        </tr>
-      </table>
-    </div>
-
-    <p style="background:lightyellow; padding: 30px;" class="text-3l"><b>Catatan:</b> Beberapa elemen HTML tidak memiliki konten (seperti elemen &lt;br&gt;). Elemen-elemen ini disebut elemen kosong. Elemen kosong tidak memiliki tag akhir!</p>
+      <p style="background:lightyellow; padding: 30px;" class="text-3l"><b>PHP adalah bahasa yang luar biasa dan populer!</b> Bahasa ini cukup kuat untuk menjadi inti dari sistem blog terbesar di web (WordPress)!
+      Bahasa ini cukup dalam untuk menjalankan jaringan sosial yang besar!
+      PHP juga cukup mudah untuk menjadi bahasa pemrograman sisi server pertama bagi pemula!</p>
     <div class="flex flex-col p-6">
+      <div class="py-4 border-b">
 
-      <div class="py-1  border-b">
+        <div class="px-3 font-bold text-3xl">Apa yang dimaksud dengan file PHP?</div>
+        <ul style="list-style-type:circle;" class="px-12 py-5">
+          <li>File PHP dapat berisi teks, HTML, CSS, JavaScript, dan kode PHP<</li>
+          <li>Kode PHP dieksekusi di server, dan hasilnya dikembalikan ke browser sebagai HTML biasa</li>
+          <li>File PHP memiliki ekstensi “.php”</li>
+         </ul>
       </div>
+      <div class="py-4 border-b">
 
-      <div class="py-6 border-b">
-      </div>
-
-      <div class="px-3 py-4 font-bold text-3xl">Web Browsers</div>
-      <div class="px-3 text-4m">Tujuan browser web (Chrome, Edge, Firefox, Safari) adalah untuk membaca dokumen HTML dan menampilkannya dengan benar.
-        Browser tidak menampilkan tag HTML, namun menggunakannya untuk menentukan cara menampilkan dokumen:</div>
-      <ul style="list-style-type:circle;" class="px-12 py-5">
-        <img src="https://www.w3schools.com/html/img_chrome.png" alt="Deskripsi Gambar">
-
+      <div class="px-3 font-bold text-3xl">Apa yang dimaksud dengan file PHP?</div>
+        <ul style="list-style-type:circle;" class="px-12 py-5">
+      <li>File PHP dapat berisi teks, HTML, CSS, JavaScript, dan kode PHP</li>
+          <li>PHP dapat membuat, membuka, membaca, menulis, menghapus, dan menutup file di server</li>
+          <li>PHP dapat mengumpulkan data formulir</li>
+          <li>PHP dapat mengirim dan menerima cookie</li>
+          <li>PHP dapat menambah, menghapus, memodifikasi data dalam database Anda</li>
+          <li>PHP dapat digunakan untuk mengontrol akses pengguna</li>
+          <li>PHP dapat mengenkripsi data</li>    
+      <div> Dengan PHP, Anda tidak terbatas pada output HTML. Anda dapat menampilkan gambar atau file PDF. Anda juga dapat menghasilkan teks apa pun, seperti XHTML dan XML.</div>
     </div>
+      <div class="py-4 border-b">
 
-  </div>
+
+      <div class="px-3 font-bold text-3xl">Mengapa PHP?</div>
+        <ul style="list-style-type:circle;" class="px-12 py-5">
+          <li>PHP berjalan di berbagai platform (Windows, Linux, Unix, Mac OS X, dll.)</li>
+          <li>PHP kompatibel dengan hampir semua server yang digunakan saat ini (Apache, IIS, dll.)</li>
+          <li>PHP mendukung berbagai macam database</li>
+          <li>PHP gratis. Unduh dari sumber daya resmi PHP: www.php.net</li>
+          <li>PHP mudah dipelajari dan berjalan secara efisien di sisi server</li>    
+      <div> Dengan PHP, Anda tidak terbatas pada output HTML. Anda dapat menampilkan gambar atau file PDF. Anda juga dapat menghasilkan teks apa pun, seperti XHTML dan XML.</div>
+    </div>
+      <div class="py-4 border-b">
+
+      <div class="px-3 font-bold text-3xl">Apa yang baru dalam PHP 7</div>
+        <ul style="list-style-type:circle;" class="px-12 py-5">
+          <li>PHP 7 jauh lebih cepat daripada rilis stabil populer sebelumnya (PHP 5.6)</li>
+          <li>PHP 7 telah meningkatkan Penanganan Kesalahan</li>
+          <li>PHP 7 mendukung Deklarasi Tipe yang lebih ketat untuk argumen fungsi</li>
+          <li>PHP 7 mendukung operator baru (seperti operator pesawat ruang angkasa: <=>)</li>
+        </div>
+      <div class="py-4 border-b">
+      </div>
   </div>
 </x-app-layout>

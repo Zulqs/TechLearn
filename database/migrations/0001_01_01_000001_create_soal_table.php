ec3807c21d147f30b3426ac11628c43aba915532
Zulqs <108636905+Zulqs@users.noreply.github.com>
2024-06-05 07:48:08 +0700

    feat: Add migration for creating 'questions' table

---
+++ database/migrations/0001_01_01_000001_create_soal_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag');
+            $table->text('quest');
+            $table->string('qanswer');
+            $table->string('qa');
+            $table->string('qb');
+            $table->string('qc');
+            $table->string('qd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};

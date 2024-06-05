51039c32e1972d9f919569eddd3a3cac2e41cbf5
Zulqs <108636905+Zulqs@users.noreply.github.com>
2024-06-06 06:56:54 +0700

    chore: Update API routing and handling for improved performance and request handling

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
 require __DIR__ . '/../public/index.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+); 
+$reponse->send();
+$kernel->terminate($request, $response);

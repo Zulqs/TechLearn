c8e3287b42d6deb50c39eb228c5715ce88ba7c44
ㄹ <luhagustinaaaryani@gmail.com>
2024-06-06 14:24:09 +0700

    update tampilan course

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -9,8 +9,7 @@
         <div class="flex flex-row flex-wrap justify-between">
 
             <!-- Module Course Card -->
-            <a href="{{url('/course/html')}}"
-            class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
+            <a href="{{url('/course/html')}}" class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
                 <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
                     <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal HTML</h2>
                     <img class="rounded-md" src="https://th.bing.com/th/id/OIP.1ixR62jPxrVMJAO8XwfSwAHaEK?w=265&h=180&c=7&r=0&o=5&pid=1.7" alt="course_img" width="70px">
@@ -24,7 +23,7 @@
                         </div>
                         <div class="flex flex-col ml-6">
                             <span>Waktu Tempuh</span>
-                            <span class="font-semibold">34 Jam</span>
+                            <span class="font-semibold">1 Jam</span>
                         </div>
                     </div>
                     <div class="text-xl font-bold text-btr py-3">Pelajari</div>
@@ -32,8 +31,7 @@
             </a>
 
             <!-- Module Course Card -->
-            <a href="{{url('/course/php')}}"
-            class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
+            <a href="{{url('/course/php')}}" class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
                 <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
                     <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal PHP</h2>
                     <img class="rounded-md" src="https://th.bing.com/th/id/OIP.JqDGE7w9t-dGwVXaXCjhUwHaEz?w=256&h=180&c=7&r=0&o=5&pid=1.7" alt="course_img" width="70px">
@@ -47,7 +45,7 @@
                         </div>
                         <div class="flex flex-col ml-6">
                             <span>Waktu Tempuh</span>
-                            <span class="font-semibold">34 Jam</span>
+                            <span class="font-semibold">1 Jam</span>
                         </div>
                     </div>
                     <div class="text-xl font-bold text-btr py-3">Pelajari</div>
@@ -55,14 +53,13 @@
             </a>
 
             <!-- Module Course Card -->
-            <a href="{{url('/course/kapasitor')}}"
-            class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
+            <a href="{{url('/course/kapasitor')}}" class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
                 <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
                     <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal Kapasitor</h2>
-                    <img class="rounded-md" src="https://cerdika.com/wp-content/uploads/2020/01/Jenis-Kapasitor-760x570.jpg" alt="course_img" width="70px">
+                    <img class="rounded-md" src="https://cerdika.com/wp-content/uploads/2020/01/Jenis-Kapasitor-760x570.jpg" alt="course_img" width="63px">
                 </div>
                 <div class="p-4">
-                    <p class="my-4">ROM merupakan salah satu dari hadware penyimpanan. Pasti perangkat kamu akan selalu membutuhkan RM. Dengan ROM penyimpanan akan aman.  </p>
+                    <p class="my-4">Kapasitor merupakan komponen elektronik yang berfungsi untuk menyimpan muatan listrik. Dengan kapasitor muatan listrik akan aman. </p>
                     <div class="flex flex-row my-4">
                         <div class="flex flex-col">
                             <span>Pembelajaran</span>
@@ -70,22 +67,21 @@
                         </div>
                         <div class="flex flex-col ml-6">
                             <span>Waktu Tempuh</span>
-                            <span class="font-semibold">34 Jam</span>
+                            <span class="font-semibold">1 Jam</span>
                         </div>
                     </div>
                     <div class="text-xl font-bold text-btr py-3">Pelajari</div>
                 </div>
             </a>
-            
+
             <!-- Module Course Card -->
-            <a href="{{url('/course/switch')}}"
-            class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
+            <a href="{{url('/course/switch')}}" class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
                 <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
                     <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal Switch</h2>
                     <img class="rounded-md" src="https://kliniktekno.com/wp-content/uploads/adtran_netvanta.jpg" alt="course_img" width="70px">
                 </div>
                 <div class="p-4">
-                    <p class="my-4">ROM merupakan salah satu dari hadware penyimpanan. Pasti perangkat kamu akan selalu membutuhkan ROM. Dengan ROM penyimpanan akan aman.  </p>
+                    <p class="my-4">Switch adalah perangkat dalam jaringan komputer yang digunakan untuk menghubungkan beberapa perangkat. Dengan switch perangkatmu akan selalu terhubung. </p>
                     <div class="flex flex-row my-4">
                         <div class="flex flex-col">
                             <span>Pembelajaran</span>
@@ -93,44 +89,21 @@
                         </div>
                         <div class="flex flex-col ml-6">
                             <span>Waktu Tempuh</span>
-                            <span class="font-semibold">34 Jam</span>
-                        </div>
-                    </div>
-                    <div class="text-xl font-bold text-btr py-3">Pelajari</div>
-                </div>
-            </a>
-            
-                                   <!-- Module Course Card -->
-            <a href="{{url('/course/router')}}"
-            class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
-                <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
-                    <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal Router</h2>
-                    <img class="rounded-md" src="https://th.bing.com/th/id/OIP.u_wqKiDVbUlnSdIHfO-AqwHaDb?w=335&h=180&c=7&r=0&o=5&pid=1.7" alt="course_img" width="70px">
-                </div>
-                <div class="p-4">
-                    <p class="my-4">ROM merupakan salah satu dari hadware penyimpanan. Pasti perangkat kamu akan selalu membutuhkan ROM. Dengan ROM penyimpanan akan aman.  </p>
-                    <div class="flex flex-row my-4">
-                        <div class="flex flex-col">
-                            <span>Pembelajaran</span>
-                            <span class="font-medium">Self Learning</span>
-                        </div>
-                        <div class="flex flex-col ml-6">
-                            <span>Waktu Tempuh</span>
-                            <span class="font-semibold">34 Jam</span>
+                            <span class="font-semibold">1 Jam</span>
                         </div>
                     </div>
                     <div class="text-xl font-bold text-btr py-3">Pelajari</div>
                 </div>
             </a>
 
-            <a href="{{url('/course/resistor')}}"
-            class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
+            <!-- Module Course Card -->
+            <a href="{{url('/course/router')}}" class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
                 <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
-                    <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal Resistor</h2>
-                    <img class="rounded-md" src="https://th.bing.com/th/id/OIP.NSQ1rH_jWePU7gP9Zj3NsgHaFL?w=316&h=180&c=7&r=0&o=5&pid=1.7" alt="course_img" width="70px">
+                    <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal Router</h2>
+                    <img class="rounded-md" src="https://th.bing.com/th/id/OIP.u_wqKiDVbUlnSdIHfO-AqwHaDb?w=335&h=180&c=7&r=0&o=5&pid=1.7" alt="course_img" width="70px">
                 </div>
                 <div class="p-4">
-                    <p class="my-4">ROM merupakan salah satu dari hadware penyimpanan. Pasti perangkat kamu akan selalu membutuhkan ROM. Dengan ROM penyimpanan akan aman.  </p>
+                    <p class="my-4">Router adalah perangkat untuk mentransmisikan paket data dari jaringan internet ke perangkat lain melalui proses routing.Dengan Router transmisi jaringan akan aman. </p>
                     <div class="flex flex-row my-4">
                         <div class="flex flex-col">
                             <span>Pembelajaran</span>
@@ -138,7 +111,29 @@
                         </div>
                         <div class="flex flex-col ml-6">
                             <span>Waktu Tempuh</span>
-                            <span class="font-semibold">34 Jam</span>
+                            <span class="font-semibold">1 Jam</span>
+                        </div>
+                    </div>
+                    <div class="text-xl font-bold text-btr py-3">Pelajari</div>
+                </div>
+            </a>
+
+            
+            <a href="{{url('/course/resistor')}}" class="flex flex-col w-[350px] border-2 rounded-lg hover:shadow-lg shadow-bgl my-5">
+                <div class="flex flex-row justify-between rounded-t-lg bg-btr p-4">
+                    <h2 class="my-auto mr-4 font-bold text-2xl text-white">Mengenal Resistor</h2>
+                    <img class="rounded-md" src="https://th.bing.com/th/id/OIP.NSQ1rH_jWePU7gP9Zj3NsgHaFL?w=316&h=180&c=7&r=0&o=5&pid=1.7" alt="course_img" width="70px">
+                </div>
+                <div class="p-4">
+                    <p class="my-4">Resistor adalah komponen pasif dalam sirkuit elektronik yang mengatur aliran arus listrik. Resistor memiliki resistansi loh. </p>
+                    <div class="flex flex-row my-4">
+                        <div class="flex flex-col">
+                            <span>Pembelajaran</span>
+                            <span class="font-medium">Self Learning</span>
+                        </div>
+                        <div class="flex flex-col ml-6">
+                            <span>Waktu Tempuh</span>
+                            <span class="font-semibold">1 Jam</span>
                         </div>
                     </div>
                     <div class="text-xl font-bold text-btr py-3">Pelajari</div>

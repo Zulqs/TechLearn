836e49d6e4a1101b6e9e9ccd0d0db7db9d253ae3
Zulqs <108636905+Zulqs@users.noreply.github.com>
2024-06-07 14:43:17 +0700

    Update navigation layout with additional links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
@@ -43,22 +43,6 @@
                                 </svg>
                             </div>
                         </button>
-                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('course')" :active="request()->routeIs('course')">
-                        {{ __('Course') }}
-                    </x-nav-link>
-                    @if (Auth::user()->role == 'admin')
-                        <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
-                            {{ __('Users') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('banksoal.index')" :active="request()->routeIs('banksoal.index')">
-                            {{ __('Bank Soal') }}
-                        </x-nav-link>
-                    @else
-                    @endif
                     </x-slot>
 
                     <x-slot name="content">

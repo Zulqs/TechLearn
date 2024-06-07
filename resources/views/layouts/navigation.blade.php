b5f26a728b410be45f9f652bd1290ffee7f3ddfd
Zulqs <108636905+Zulqs@users.noreply.github.com>
2024-06-07 14:45:50 +0700

    Update navigation layout with additional links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,6 +27,15 @@
                         </x-nav-link>
                     @else
                     @endif
+                    <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                    </form>
                 </div>
             </div>
 
